4012ea9d045ad056c209a3ef9d8ac5052c8e2927
Zain Jakwani <zjakwani@gmail.com>
2022-06-12 15:19:45 -0700

    modularized the main loop

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 # MVP app for a chef keeping track of dishes
 
+# global list of dishes
 dishes = ["Pancakes", "Waffles"]
 
-print("Welcome to my chef app")
+
+# string for the menu of commands
+cmd_menu = """\nSelect from the following commands:
+        a. Add dish
+        b. Delete dish
+        c. Quit\n"""
+
+
+# Outputs all dishes
+def output_dishes():
+    print("\nList of dishes:")
+    for i in range(len(dishes)):
+        print(f"\t{str(i+1)}. {dishes[i]}")
+
 
 # Basic CLI
-while True:
+def main():
+    print("Welcome to my chef app")
+    
+    while True:
 
-    # Outputs all dishes
-    print("\nList of dishes:")
-    for i in range(len(dishes)): print(f"\t{str(i+1)}. {dishes[i]}")
+        output_dishes()
+        print(cmd_menu)
 
-    # Command menu
-    print("\nSelect from the following commands")
-    print("\ta. Add dish")
-    print("\tb. Delete dish")
-    print("\tc. Quit\n")
-    cmd = input("Command: ")
+        # Switch-like statement for each command
+        cmd = input("Command: ")
 
+        if cmd == "a":
+            name = input("Name of dish: ")
+            dishes.append(str(name))
 
-    # Switch-like statement for each command
-    if cmd == "a":
-        name = input("Name of dish: ")
-        dishes.append(str(name))
+        elif cmd == "b":
+            remove_index = input("Number of dish to be removed: ")
 
-    elif cmd == "b":
-        remove_index = input("Number of dish to be removed: ")
+            if not remove_index.isnumeric() or int(remove_index)-1 < 0 or int(remove_index)-1 >= len(dishes):
+                print("Error: please enter a valid dish")
+            else:
+                dishes.pop(int(remove_index) - 1)
 
-        if not remove_index.isnumeric() or int(remove_index)-1 < 0 or int(remove_index)-1 >= len(dishes):
-            print("Erorr: please enter a valid dish")
         else:
-            dishes.pop(int(remove_index) - 1)
+            break
 
-    else:
-        break
+
+if __name__ == "__main__":
+    main()
